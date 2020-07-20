f91df8c35e6ffa9565e377102310e19717b5894a
Kimhohan <ghgksdk123@gmail.com>
2020-07-20 15:56:51 +0900

    엔티티 설계/ S03P12D105-12

--- SNS_Backend/src/main/java/com/ssafy/pjt1/dao/AuthDao.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.pjt1.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.pjt1.dto.Auth;
-
-public interface AuthDao extends JpaRepository<Auth, String> {
-
-}
--- SNS_Backend/src/main/java/com/ssafy/pjt1/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.pjt1.service;
-
-import com.ssafy.pjt1.dto.Auth;
-
-public interface AuthService {
-	void insert(Auth auth);
-}
