bac289f3cc5bc894c3e162c811e7d45947f733c9
yusoojeong <yusj1231@gmail.com>
2020-07-21 15:19:38 +0900

    added studydetail / S03P12D105-19

--- frontend/src/views/SNS/IndexFeed.vue
+++ frontend/src/views/SNS/IndexFeed.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="feed newsfeed">
     <div class="wrapB">
+      <Navbar></Navbar>
+      <subNav></subNav>
       <h1>뉴스피드</h1>
 
       <FeedItem />
@@ -16,11 +18,17 @@ import { mapState } from "vuex";
 import "../../components/css/feed/feed-item.scss";
 import "../../components/css/feed/newsfeed.scss";
 import FeedItem from "../../components/SNS/FeedItem.vue";
+import Navbar from '../../components/common/Navigation.vue'
+import subNav from '../../components/common/subnav.vue'
 
 export default {
   props: ["keyword"],
 
-  components: { FeedItem }
+  components: { 
+    FeedItem,
+    Navbar,
+    subNav,
+  },
 };
 </script>
 
