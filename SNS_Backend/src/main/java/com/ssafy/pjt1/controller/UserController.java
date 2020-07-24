81b2698d2b3060a52a0ab6d00e7fa8b9d1e6ddd4
Daeheum <conquerer1209@gmail.com>
2020-07-24 12:08:05 +0900

    회원가입/비밀번호 찾기/이메일 인증

--- SNS_Backend/src/main/java/com/ssafy/pjt1/controller/UserController.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/controller/UserController.java
@@ -68,11 +68,28 @@ public class UserController {
 
 	@PostMapping("/account/loginMailSend")
 	@ApiOperation(value = "회원가입시 인증 메일 전송", notes = "회원가입시 인증 메일 전송 기능 구현")
-	public void loginMailSend(@Valid @RequestBody String email) throws MessagingException {
-		// 이메일 보내기
-		System.out.println(email);
-		System.out.println(email.charAt(0));
-		customMailSender.sendMail(email);
+	public Object loginMailSend(@Valid @RequestBody String email) throws MessagingException {
+		// 이메일 중복 체크
+		Optional<User> user = userdao.findUserByEmail(email);
+		// User u = user.get();
+		// System.out.println(u.getEmail());
+		if (user.isPresent()) {
+			System.out.println("이메일 중복");
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "email duplicate!!";
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+		} else {
+			// 이메일 보내기
+			System.out.println(email);
+			System.out.println(email.charAt(0));
+			customMailSender.SingUpSendMail(email);
+
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "success";
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		}
 	}
 
 	@PostMapping("/account/loginMailConfirm")
@@ -98,15 +115,83 @@ public class UserController {
 				return new ResponseEntity<>(result, HttpStatus.OK);
 			} else {
 				System.out.println("실패");
+				final BasicResponse result = new BasicResponse();
+				result.status = true;
+				result.data = "인증번호가 다릅니다";
 				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 			}
 		} else {
-			System.out.println("실패");
+			System.out.println("인증을 안받았습니다");
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "인증번호가 없습니다";
 			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 		}
 	}
 
-	// Create
+	@PostMapping("/account/passwordUpdateMailSend")
+	@ApiOperation(value = "비밀번호 변경시 인증 메일 전송", notes = "비밀번호 변경시 인증 메일 전송 기능 구현")
+	public Object passwordUpdateMailSend(@Valid @RequestBody String email) throws MessagingException {
+		// 이메일 중복 체크
+		Optional<User> user = userdao.findUserByEmail(email);
+		// User u = user.get();
+		// System.out.println(u.getEmail());
+		if (!user.isPresent()) {
+			System.out.println("이메일이 없습니다.");
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "없는 이메일입니다.";
+			return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+		} else {
+			// 이메일 보내기
+			System.out.println(email);
+			System.out.println(email.charAt(0));
+			customMailSender.passwordUpdateSendMail(email);
+
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "success";
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		}
+	}
+
+	@PostMapping("/account/passwordUpdateMailConfirm")
+	@ApiOperation(value = "비밀번호 변경시 메일인증", notes = "비밀번호 변경시 메일인증 기능 구현")
+	public Object passwordUpdateMailConfirm(@Valid @RequestBody Auth auth) {
+		// 확인 하기
+		System.out.println(auth.getAuth_email());
+		System.out.println(auth.getAuth_number());
+
+		Optional<Auth> flag = authservice.findone(auth.getAuth_email());
+
+		flag.ifPresent(selectUser -> {
+			num = selectUser.getAuth_number();
+		});
+
+		if (flag != null) {
+			if (num.equals(auth.getAuth_number())) {
+				final BasicResponse result = new BasicResponse();
+				result.status = true;
+				result.data = "success";
+				System.out.println(num);
+				return new ResponseEntity<>(result, HttpStatus.OK);
+			} else {
+				System.out.println("실패");
+				final BasicResponse result = new BasicResponse();
+				result.status = true;
+				result.data = "인증번호가 다릅니다";
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			}
+		} else {
+			System.out.println("인증을 안받았습니다");
+			final BasicResponse result = new BasicResponse();
+			result.status = true;
+			result.data = "인증번호가 없습니다";
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
+
+// Create
 	@PostMapping("/account/signup")
 	@ApiOperation(value = "가입하기", notes = "가입하기 기능을 구현")
 
