512cb071c842891555b0509f298781b7068eeb9e
Daeheum <conquerer1209@gmail.com>
2020-07-29 18:22:40 +0900

    회원삭제기능

--- SNS_Backend/src/main/java/com/ssafy/pjt1/dto/Tag.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/dto/Tag.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-
 @Entity
 @Table(name = "Tag")
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -27,19 +26,19 @@ public class Tag {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@JsonIgnore
 	private int tid;
-	
+
 	private String name;
 
-	@ManyToMany(mappedBy = "tags")
+	@ManyToMany(mappedBy = "tags", cascade = CascadeType.REMOVE)
 	private Set<User> users;
-	
+
 	@ManyToMany(mappedBy = "tags")
 	private Set<Post> posts;
-	
+
 	public Tag() {
-	
+
 	}
-	
+
 	public Tag(String name) {
 		this.name = name;
 		this.users = new HashSet<User>();
@@ -69,7 +68,7 @@ public class Tag {
 	public void setUsers(Set<User> users) {
 		this.users = users;
 	}
-	
+
 	public Set<Post> getPosts() {
 		return posts;
 	}
@@ -77,7 +76,7 @@ public class Tag {
 	public void setPosts(Set<Post> posts) {
 		this.posts = posts;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		Tag t = (Tag) obj;
@@ -92,5 +91,4 @@ public class Tag {
 	public int hashCode() {
 		return Objects.hash(tid);
 	}
-	
 }
