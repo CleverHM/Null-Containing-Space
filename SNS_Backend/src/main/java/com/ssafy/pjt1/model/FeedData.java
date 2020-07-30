f2cd1f71d15bf50c0b8226a111066c73fcfe67f2
Kimhohan <ghgksdk123@gmail.com>
2020-07-30 21:40:46 +0900

    Like, post 기능 추가

--- SNS_Backend/src/main/java/com/ssafy/pjt1/model/FeedData.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/model/FeedData.java
@@ -1,90 +1,91 @@
 package com.ssafy.pjt1.model;
 
+import java.io.File;
 import java.util.List;
 
 public class FeedData {
-	private int pid;
-	private String email;
-	private String date;
-	private String title;
-	private String userName;
-	private String fileName;
-	private String fileUrl;
-	private List<String> tags;
+    private int pid;
+    private int LikeCount;
+    private String email;
+    private String date;
+    private String title;
+    private String userName;
+    private byte[] file;
+    private List<String> tags;
 
-	public FeedData(int pid, String email, String date, String title,String userName, String fileName, String fileUrl, List<String> tags) {
-		this.pid = pid;
-		this.email = email;
-		this.date = date;
-		this.title = title;
-		this.userName = userName;
-		this.fileName = fileName;
-		this.fileUrl = fileUrl;
-		this.tags = tags;
+    public FeedData(int pid, String email, String date, String title,String userName, byte[] file, List<String> tags, int likeCount) {
+        this.pid = pid;
+        this.email = email;
+        this.date = date;
+        this.title = title;
+        this.userName = userName;
+        this.file = file;
+        this.tags = tags;
+        this.LikeCount = likeCount;
+    }
+
+    public int getLikeCount() {
+		return LikeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		LikeCount = likeCount;
 	}
 
 	public String getDate() {
-		return date;
-	}
+        return date;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public int getPid() {
-		return pid;
-	}
+    public int getPid() {
+        return pid;
+    }
 
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public byte[] getFile() {
+        return file;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
 
-	public String getFileName() {
-		return fileName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getFileUrl() {
-		return fileUrl;
-	}
+    public List<String> getTags() {
+        return tags;
+    }
 
-	public void setFileUrl(String fileUrl) {
-		this.fileUrl = fileUrl;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 
 }
