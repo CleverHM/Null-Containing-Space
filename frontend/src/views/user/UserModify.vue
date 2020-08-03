8b185fa393bc71609d50fce4cd12fde8e2f5e4c1
김지영 <jiyoung112837@gmail.com>
2020-08-03 13:17:55 +0900

    Make User Modify page

---
+++ frontend/src/views/user/UserModify.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <subNav></subNav>
+    </div>
+</template>
+
+<script>
+import Navbar from '../../components/common/Navigation.vue'
+import subNav from '../../components/common/subnav.vue'
+export default {
+    name: 'ModifyUser',
+    components: {
+        Navbar,
+        subNav,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
