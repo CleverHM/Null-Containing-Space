9e89dace0cf6b657b5f5a42037391da2dfb82c83
park03851 <park03851@naver.com>
2020-08-04 17:51:46 +0900

    다시 받아오기

--- SNS_Backend/src/main/java/com/ssafy/pjt1/service/MatchingService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.pjt1.service;
-
-import java.util.List;
-
-import com.ssafy.pjt1.dto.User;
-
-public interface MatchingService {
-	public List<Integer> match(List<String> back, List<String> front, List<String> database, List<String> frame, String algo);
-}
--- SNS_Backend/src/main/java/com/ssafy/pjt1/service/PostService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.pjt1.service;
-
-import java.util.Optional;
-
-import com.ssafy.pjt1.dto.Post;
-
-public interface PostService {
-	Optional<Post> findone(int postid);
-}
