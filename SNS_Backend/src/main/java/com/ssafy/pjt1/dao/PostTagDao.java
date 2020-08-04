fc969f5813f357a5a12e1c018fcf14e16a0aef1d
Kimhohan <ghgksdk123@gmail.com>
2020-08-04 10:02:15 +0900

    post update

--- SNS_Backend/src/main/java/com/ssafy/pjt1/dao/PostTagDao.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/dao/PostTagDao.java
@@ -12,5 +12,5 @@ public interface PostTagDao extends JpaRepository<PostTag, Integer> {
 	@Transactional
     @Modifying
     @Query(value="delete from posttag where post_id=:pid", nativeQuery=true)
-    void deleteUser2(@Param("pid") int pid);
+    void deletePosttag(@Param("pid") int pid);
 }
