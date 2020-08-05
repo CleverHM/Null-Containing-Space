af7ca940860df9dd4b3dc5baeb135a46575dbcd7
김지영 <jiyoung112837@gmail.com>
2020-08-05 15:54:59 +0900

    start step1 in join page / S03P13D105-41

--- frontend/src/components/user/step1.vue
+++ frontend/src/components/user/step1.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="join1">
-      <h5 class="mb-4" v-if="linkName==='Join'">
+      <h5 class="mb-4" v-if="linkName==='step1'">
       {{ title.join }}
       </h5>
       <h5 class="mb-4" v-if="linkName==='findPassword'">
       {{ title.password }}
       </h5>
-      <div class="form-wrap">
-        <div class="input-with-label">
-          <input @keyup.enter="confirmEmail" v-model="childemail" id="email" placeholder="이메일을 입력하세요." type="text" />
+      <div class="input-form">
+          <input @keyup.enter="confirmEmail" v-model="email" id="email" placeholder="이메일을 입력하세요." type="text" />
           <label for="email">이메일</label>
           <span id="ErrorMsg">{{ ErrorMessage }}</span>
-
-        </div>
       </div>
       
       <button class="btn-input" @click="confirmEmail">입력</button>
@@ -21,34 +18,29 @@
 
 <script>
 import http from "@/util/http-common.js";
-import "../../assets/css/components.scss";
 // 이메일 체크 정규식
 var EmailregExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 export default {
-  props: {
-    ErrorMessage:{
-      type:String,
-    }
-  },
   created() {
     this.linkName = this.$route.name
-    console.log(this.linkName)
   },
   data : () => {
     return {
       linkName: null,
+      email: "",
       title : {
         join : "가입할 이메일 주소를 아래에 입력해주세요.",
         password : "가입한 이메일 주소를 아래에 입력해주세요.",
       },
-      childemail: "",
+      ErrorMessage: "",
+      
     }
   },
   methods:{
     // 데이터베이스에 이메일이 있는지 확인하고 다음페이지로 이동시키기
     confirmEmail() {
-        if (this.childemail.match(EmailregExp) != null){
-          this.$emit("ConfirmEmail", this.childemail)
+        if (this.email.match(EmailregExp) != null){
+          this.$route.push({name: 'step2', params: {email: this.email}})
         }else {
           this.ErrorMessage = "이메일 형식이 올바르지 않습니다. 다시 입력해주세요."
         }
@@ -66,10 +58,30 @@ export default {
   top:134px;
   padding: 0px 30px 30px 30px;
 }
-.input-with-label{
-  width: 100%;
+.input-form {
+    position: relative;
+    height: 50px;
+}
+input[type="text"]{
+    padding-left: 100px;
+    width: 100%;
+    border: 1px solid #464545;
+    border-radius: 3px;
+}
+input[type="text"]:focus{
+    border: 0;
+    border-bottom: 1.2px solid #464545;
 }
 
+.input-form label {
+    position: absolute;
+    padding: 0;
+    margin: 0;
+    font-size: 13px;
+    font-weight: bold;
+    top: 17px; 
+    left: 13px;
+}
 .btn-input{
   position: fixed;
   bottom:0;
