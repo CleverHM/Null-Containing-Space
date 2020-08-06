08ac901152c2ba2ede78714b720cca4d6bb76909
Kimhohan <ghgksdk123@gmail.com>
2020-08-06 18:05:52 +0900

    match boolean 수정

--- SNS_Backend/src/main/java/com/ssafy/pjt1/dto/User.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/dto/User.java
@@ -7,13 +7,10 @@ import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -23,7 +20,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.google.common.hash.BloomFilter;
 
 @Entity
 @Table(name = "User")
