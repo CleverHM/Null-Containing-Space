02eabe7f4c59457d5fd081081e4eb3cbe26ba6e4
Kimhohan <ghgksdk123@gmail.com>
2020-08-08 21:49:32 +0900

    배포용

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@
 
 // axios 객체 생성
 export default axios.create({
-    baseURL:"http://localhost:8080", 
+     baseURL: 'http://52.79.249.53:8080',
     headers: {
       'Content-type': 'application/json',
     },
