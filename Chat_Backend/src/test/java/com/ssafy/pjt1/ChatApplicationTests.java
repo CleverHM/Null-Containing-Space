cd89172bc90f3bc650c924e3f48d5e2684a1162c
Daeheum <conquerer1209@gmail.com>
2020-08-08 20:56:56 +0900

    채팅 서버 구축중

---
+++ Chat_Backend/src/test/java/com/ssafy/pjt1/ChatApplicationTests.java
@@ -0,0 +1,20 @@
+package com.ssafy.pjt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.MessagingException;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Autowired
+	
+//	user1 = new User();
+	
+	@Test
+	void sendMail() throws MessagingException {
+		//customMailSender.sendMail();
+	}
+
+}
