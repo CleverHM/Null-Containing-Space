168c3beb1ce7d28cfb40dfc6890f70830f340411
Kimhohan <ghgksdk123@gmail.com>
2020-08-08 21:50:33 +0900

    수정

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,11 +2,7 @@
 
 // axios 객체 생성
 export default axios.create({
-<<<<<<< HEAD
      baseURL: 'http://52.79.249.53:8080',
-=======
-    baseURL:"http://52.79.249.53:8080", 
->>>>>>> 7b2eb19da29ccf08797d7fba587d2f2c986c69d9
     headers: {
       'Content-type': 'application/json',
     },
