bc03b299a17b5c848554a06ff75a2e951796e2ef
김지영 <jiyoung112837@gmail.com>
2020-08-10 19:57:13 +0900

    start making myPostItem / S03P13D105-60

--- frontend/src/components/user/myPostItem.vue
+++ frontend/src/components/user/myPostItem.vue
@@ -1,12 +1,67 @@
 <template>
-    <div>
-        sdfsdf
+    <div class="myPostItem">
+        <!-- post image -->
+        <div class="mypost-image">
+
+        </div>
+        <div class="mypost-info">
+            <!-- title -->
+            <div class="mypost-title"></div>
+            <!-- 조회수, 좋아요, 댓글 -->
+            <div class="mypost-etc">
+                <!-- 조회수 -->
+                <div class="mypost-view"></div>
+                <!-- 좋아요 -->
+                <div class="mypost-like"></div>
+                <!-- 댓글 -->
+                <div class="mypost-comment"></div>
+
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+var now = new Date(); // 현재 시간 받아오기
+
 export default {
-    name: 'myPostItem',    
+    name: 'myPostItem',
+    method: {
+        // 날짜 체크
+        dateCheck(date) {
+        var diff = now - date
+        var diff_sec = Math.floor(diff / 1000)
+        var diff_min = Math.floor(diff_sec / 60)
+        var diff_hour = Math.floor(diff_min / 60)
+        var diff_day = Math.floor(diff_hour / 24)
+        var diff_month = Math.floor(diff_day / 30)
+        var diff_year = Math.floor(diff_month / 12)
+
+        if (diff_year > 0) {
+            var calyear = diff_year + '년 전'
+            return calyear
+        } else if (diff_month > 0) {
+            var calmonth = diff_month + '달 전'
+            return calmonth
+        } else if (diff_day > 0) {
+            var calday = diff_day + '일 전'
+            return calday
+        } else if (diff_hour > 0) {
+            var calhour = diff_hour + '시간 전'
+            return calhour
+        } else if (diff_min > 0) {
+            var calmin = diff_min + '분 전'
+            return calmin
+        } else if(diff_sec > 0) {
+            var caltime = diff_sec + '초 전'
+            return caltime
+        } else {
+            return '0초 전'
+        }
+        },
+    }
+    
 }
 </script>
 
--- frontend/src/views/user/myPost.vue
+++ frontend/src/views/user/myPost.vue
@@ -1,16 +1,50 @@
 <template>
-    <div>
+    <div v-if="isLoading"></div>
+    <div v-else>
+        <Navbar />
         <myPostItem></myPostItem>
     </div>
 </template>
 
 <script>
+import Navbar from '@/components/common/Navigation.vue'
 import myPostItem from '@/components/user/myPostItem.vue'
+import http from '@/util/http-common.js'
 export default {
     name: 'myPost',
     components : {
+        Navbar,
         myPostItem,
-    }
+    },
+    props: [
+        'nickname',
+    ],
+    created() {
+        var InputData = new FormData()
+        InputData.append("nickname", this.nickname)
+        http.post("/post/myPost", InputData)
+        .then(({data}) => {
+            console.log(data)
+        })
+        this.Loading()
+    },
+    data() {
+        return {
+            isLoading: true,
+        }
+    },
+    methods: {
+        Loading() {
+              if (this.isLoading) {
+                  setTimeout(this.delayfinish, 100);
+              }
+        },
+          // 딜레이 화면
+        delayfinish(){
+              this.isLoading = false;
+        },
+
+    }    
 }
 </script>
 
