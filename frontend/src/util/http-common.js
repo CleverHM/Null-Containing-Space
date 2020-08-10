442d946c3e89ee620dcfd578b8ee16413e640ebf
Kimhohan <ghgksdk123@gmail.com>
2020-08-10 13:08:21 +0900

    front 댕기기

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,11 +2,7 @@
 
 // axios 객체 생성
 export default axios.create({
-<<<<<<< HEAD
-     baseURL: 'http://localhost:8080',
-=======
     baseURL:"http://localhost:8080", 
->>>>>>> 81e8a4ceee19f50975a898f1cc0a761e2dd05b6a
     headers: {
       'Content-type': 'application/json',
     },
