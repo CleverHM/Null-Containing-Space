d0ea3bda11a1a4a97d6c56fecf716df4357311cf
Daeheum <conquerer1209@gmail.com>
2020-08-11 10:50:56 +0900

    실시간 채팅 구현중

--- SNS_Backend/src/main/java/com/ssafy/pjt1/config/WebSocketConfig.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/config/WebSocketConfig.java
@@ -1,29 +1,22 @@
 package com.ssafy.pjt1.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@RequiredArgsConstructor
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
-
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/sub");
-		config.setApplicationDestinationPrefixes("/pub");
-	}
-
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic");
+		registry.setApplicationDestinationPrefixes("/app");
 	}
 	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/websocket").withSockJS();
+	}
 }
--- SNS_Backend/src/main/java/com/ssafy/pjt1/controller/ChatController.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/controller/ChatController.java
@@ -1,21 +1,28 @@
-//package com.ssafy.pjt1.controller;
-//
-//import com.ssafy.pjt1.dto.ChatMessage;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.simp.SimpAnnotationMethodMessageHandler;
-//import org.springframework.stereotype.Controller;
-//
-//@RequiredArgsConstructor
-//@Controller
-//public class ChatController {
-//	
-//	private final SimpAnnotationMethodMessageHandler messagingTemplate;
-//	
-//	@MessageMapping("/chat/message")
-//	public void message(ChatMessage message) {
-//        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
-//            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-//        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-//    }
-//}
+package com.ssafy.pjt1.controller;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.ssafy.pjt1.dto.Chat;
+import com.ssafy.pjt1.dto.Greeting;
+import com.ssafy.pjt1.dto.HelloMessage;
+
+
+@Controller
+public class ChatController {
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(100); // delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+	
+	@MessageMapping("/chat")
+	@SendTo("/topic/chat")
+	public Chat chat(Chat chat) throws Exception {
+		return new Chat(chat.getName(), chat.getMessage());
+	}
+}
