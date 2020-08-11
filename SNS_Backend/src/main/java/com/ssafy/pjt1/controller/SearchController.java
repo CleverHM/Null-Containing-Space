1b7e7275e7b9a3db800de1baf873c6dd63cc268b
Kimhohan <ghgksdk123@gmail.com>
2020-08-11 14:56:52 +0900

    계정 찾기 수정

--- SNS_Backend/src/main/java/com/ssafy/pjt1/controller/SearchController.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/controller/SearchController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.validation.Valid;
 
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.pjt1.dao.TagDao;
 import com.ssafy.pjt1.dto.Tag;
 import com.ssafy.pjt1.dto.User;
+import com.ssafy.pjt1.dto.UserFollow;
 import com.ssafy.pjt1.model.BasicResponse;
 import com.ssafy.pjt1.model.PersonData;
 import com.ssafy.pjt1.service.UserService;
@@ -39,61 +41,96 @@ public class SearchController {
 
 	@Autowired
 	UserService userservice;
-	
+
 	@Autowired
 	TagDao tagdao;
-	
+
 	// 계정 검색 검색어 포함된 리스트
 	@PostMapping("/search/user")
 	@ApiOperation(value = "계정 검색", notes = "계정 검색 기능을 구현.")
 	public Object postDelete(@Valid @RequestParam String search, String mynickname) throws IOException {
 		List<PersonData> list = new LinkedList<PersonData>();
-		
+
 		List<User> allUser = userservice.findall();
-		
-		for(User u : allUser) {
-			if(u.getNickname().equals(mynickname)) {
-				continue;
-			}
-			else if(u.getNickname().contains(search)) {
-				
+
+		for (User u : allUser) {
+			if (u.getNickname().contains(search)) {
+
 				byte[] reportBytes1 = null;
-				File result1 = new File(u.getProfile().getFileurl()
-						+ u.getProfile().getFilename());
+				File result1 = new File(u.getProfile().getFileurl() + u.getProfile().getFilename());
 
 				if (result1.exists()) {
 					System.out.println("있음");
-					InputStream inputStream1 = new FileInputStream(u.getProfile().getFileurl()
-							+ u.getProfile().getFilename());
+					InputStream inputStream1 = new FileInputStream(
+							u.getProfile().getFileurl() + u.getProfile().getFilename());
 					byte[] out1 = org.apache.commons.io.IOUtils.toByteArray(inputStream1);
-					
-					// 팔로우 여부 확				
-					PersonData pd = new PersonData(out1, u.getNickname(), 0);
-					list.add(pd);
+
+					if (u.getNickname().equals(mynickname)) {
+						PersonData pd = new PersonData(out1, u.getNickname(), 3);
+						list.add(pd);
+					} else {
+						// 팔로우 여부 확인
+						Set<UserFollow> uf = u.getFollowings();
+
+						int f = 0;
+						end: for (UserFollow userfollow : uf) {
+							if (userfollow.getTo().getNickname().equals(mynickname)) {
+								f = 1;
+								break end;
+							}
+						}
+						if (f == 0) {
+							PersonData pd = new PersonData(out1, u.getNickname(), 0);
+							list.add(pd);
+						} else {
+							PersonData pd = new PersonData(out1, u.getNickname(), 1);
+							list.add(pd);
+						}
+					}
 				} else {
-					PersonData pd = new PersonData(reportBytes1, u.getNickname(), 0);
-					list.add(pd);
+					if (u.getNickname().equals(mynickname)) {
+						PersonData pd = new PersonData(reportBytes1, u.getNickname(), 3);
+						list.add(pd);
+					} else {
+						// 팔로우 여부 확인
+						Set<UserFollow> uf = u.getFollowings();
+
+						int f = 0;
+						end: for (UserFollow userfollow : uf) {
+							if (userfollow.getTo().getNickname().equals(mynickname)) {
+								f = 1;
+								break end;
+							}
+						}
+						if (f == 0) {
+							PersonData pd = new PersonData(reportBytes1, u.getNickname(), 0);
+							list.add(pd);
+						} else {
+							PersonData pd = new PersonData(reportBytes1, u.getNickname(), 1);
+							list.add(pd);
+						}
+					}
 				}
 			}
 		}
-		
+
 		return list;
 	}
-	
+
 	// 해쉬태그 검색 검색어 포함된 리스트
 	@PostMapping("/search/hashtag")
 	@ApiOperation(value = "hashtag 검색", notes = "hashtag 기능을 구현.")
 	public Object hashtag(@Valid @RequestParam String hashtag) throws IOException {
 		List<String> list = new LinkedList<String>();
-		
+
 		List<Tag> allTag = tagdao.findAll();
-		
-		for(Tag t : allTag) {
-			if(t.getName().contains(hashtag)) {
+
+		for (Tag t : allTag) {
+			if (t.getName().contains(hashtag)) {
 				list.add(t.getName());
 			}
 		}
-		
+
 		return list;
 	}
 }
