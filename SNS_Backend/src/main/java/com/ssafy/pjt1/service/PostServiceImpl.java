b570d712c3206e28e569d154a685dc1cd5967745
Kimhohan <ghgksdk123@gmail.com>
2020-08-11 13:25:14 +0900

    해쉬태그 검색, 해쉬태그 게시불러 오기 구현

--- SNS_Backend/src/main/java/com/ssafy/pjt1/service/PostServiceImpl.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/service/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.pjt1.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,11 @@ public class PostServiceImpl implements PostService {
 	@Override
 	public Optional<Post> findone(int postid){
 		return postdao.findPostByPid(postid);	
+	}
+
+	@Override
+	public List<Post> findall() {
+		// TODO Auto-generated method stub
+		return postdao.findAll();
 	}
 }
