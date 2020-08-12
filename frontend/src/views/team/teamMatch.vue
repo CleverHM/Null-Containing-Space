97f198d70564d54ad1c14de2678ce1e85ea162c6
park03851 <park03851@naver.com>
2020-08-12 15:07:38 +0900

    front 로딩화면 구현

--- frontend/src/views/team/teamMatch.vue
+++ frontend/src/views/team/teamMatch.vue
@@ -2,26 +2,40 @@
     <div>
         <Navbar></Navbar>
         <subNav></subNav>
-        <div class="contentArea">
+        
             <!-- 로딩 페이지 -->
-            <div v-if="loadingOn">
-                <!-- <img src="@/assets/images/lionLoading.gif" alt="" style="width:100%;"> -->
+            <div class="loadingpage" v-if="loadingOn">
+                <div class="loading">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>A</span>
+                    <span>D</span>
+                    <span>I</span>
+                    <span>N</span>
+                    <span>G</span>
+                    <span>.</span>
+                    <span>.</span>
+                    <span>.</span>
+                </div>
+                
+                <img class="loadingimg" src="@/assets/images/bear.gif" alt="">
             </div>
 
             <!-- 매칭 간략화 화면 -->
             <div v-else>
-                <div class="memberImg-area">
-                    <MatchUserSmall v-for="member in matchData.prefermember" :key="member.nickname" :userData="member" class="m-2"></MatchUserSmall>
-                    <MatchUserSmall v-for="member in matchData.noprefermember" :key="member.nickname" :userData="member" class="m-2"></MatchUserSmall>
+                <div class="contentArea">
+                    <div class="memberImg-area">
+                        <MatchUserSmall v-for="member in matchData.prefermember" :key="member.nickname" :userData="member" class="m-2"></MatchUserSmall>
+                        <MatchUserSmall v-for="member in matchData.noprefermember" :key="member.nickname" :userData="member" class="m-2"></MatchUserSmall>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button class="btnForm" @click="memberDetailOn">자세히 보기</button>
+                        <button class="btnForm ml-2" @click="reMatchOn">다시 매칭하기</button>
+                    </div>
                 </div>
-                <div class="d-flex justify-content-center">
-                    <button class="btnForm" @click="memberDetailOn">자세히 보기</button>
-                    <button class="btnForm ml-2" @click="reMatchOn">다시 매칭하기</button>
-                </div>
-
 
             </div>
-        </div>
+        
     </div>
 </template>
 
@@ -60,7 +74,7 @@ export default {
         // 로딩하는가?
         isLoading() {
             if (this.loadingOn) {
-                setTimeout(this.delayfinish, 1000);
+                setTimeout(this.delayfinish, 5000);
             }
         },
 
@@ -112,6 +126,7 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap');
 
 *:focus { outline:none; }
 
@@ -134,7 +149,71 @@ export default {
     margin-left: 50%;
     margin-top: 0%;
     transform:translate(-40%, 0%);
-
 }
+
+.loading{
+    padding-top: 20%;
+    position: relative;
+    font-family: 'Amatic SC', cursive;
+    text-align: center;
+    font-size: 50px;
+    color: white;
+    opacity: 0.7;
+}
+
+.loading span {
+  display: inline-block;
+  margin: 0 -.05em;
+  animation: loading .7s infinite;
+  padding: 1px 2px 3px 4px;
+}
+.loading span:nth-child(2) {
+  animation-delay: .1s;
+}
+.loading span:nth-child(3) {
+  animation-delay: .2s;
+}
+.loading span:nth-child(4) {
+  animation-delay: .3s;
+}
+.loading span:nth-child(5) {
+  animation-delay: .4s;
+}
+.loading span:nth-child(6) {
+  animation-delay: .5s;
+}
+.loading span:nth-child(7) {
+  animation-delay: .6s;
+}
+.loading span:nth-child(8) {
+  animation-delay: .7s;
+}
+.loading span:nth-child(9) {
+  animation-delay: .8s;
+}
+.loading span:nth-child(10) {
+  animation-delay: .9s;
+}
+@keyframes loading {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(5px);
+  }
+}
+
+.loadingpage{
+    background-color:#1c272a;
+    filter: grayscale(80%) opacity(0.6); 
+    height:600px;
+}
+
+.loadingimg{
+    margin-top: 20.5%;
+    width:100%;
+    filter: grayscale(80%) opacity(0.6); 
+}
+.loadingimg:hover { filter: none; }
 
 </style>
