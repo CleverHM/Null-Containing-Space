f192b78bcc80fa6d083af6ecefb750fbdefad219
yusoojeong <yusj1231@gmail.com>
2020-08-13 17:53:54 +0900

    infinite loading likepost and mypost page / S03P13D105-57

--- frontend/src/views/user/myPost.vue
+++ frontend/src/views/user/myPost.vue
@@ -1,8 +1,23 @@
 <template>
-    <div v-if="isLoading"><Navbar /></div>
-    <div v-else class="myPostView">
+    <div>
         <Navbar />
-        <myPostItem :post="post" v-for="post in Posts" :key="post.pid"></myPostItem>
+        <div v-if="isLoading"></div>
+
+        <div v-else class="myPostView">
+            <myPostItem :post="post" v-for="post in Posts" :key="post.pid"></myPostItem>
+        </div>
+
+        <infinite-loading 
+        @infinite="infiniteHandler" 
+        ref="InfiniteLoading"
+        spinner="waveDots">
+            <div slot="no-results" class="null-area d-flex justify-content-center align-items-center align-content-center flex-column">
+                <b-icon-envelope-open scale="1.5" font-scale="1.5" class="mb-4"/>
+                작성한 글이 없습니다.
+            </div>
+            <div slot="no-more" style="font-size: 14px; padding: 10px 0px;">더 이상 작성한 글이 존재하지 않습니다.</div>
+        </infinite-loading>
+
     </div>
 </template>
 
@@ -10,32 +25,55 @@
 import Navbar from '@/components/common/Navigation.vue'
 import myPostItem from '@/components/user/myPostItem.vue'
 import http from '@/util/http-common.js'
+import InfiniteLoading from 'vue-infinite-loading'
+
+
 export default {
     name: 'myPost',
     components : {
         Navbar,
         myPostItem,
+        InfiniteLoading,
     },
     props: [
         'nickname',
     ],
     created() {
-        var InputData = new FormData()
-        InputData.append("nickname", this.nickname)
-        http.post("/post/myPost", InputData)
-        .then(({data}) => {
-            this.Posts = data
-            console.log(this.Posts)
-        })
         this.Loading()
     },
     data() {
         return {
             isLoading: true,
             Posts: [],
+            limit: 1,
         }
     },
     methods: {
+
+         infiniteHandler($state) {
+            var EACH_LEN = 10;
+
+            var InputData = new FormData();
+            InputData.append("nickname", this.nickname);
+            InputData.append("pagenum", this.limit);
+            http.post("/post/myPost", InputData)
+            .then((res) => {
+                setTimeout(() => {
+                if(res.data.length) {
+                    this.Posts = [...this.Posts, ...res.data]
+                    $state.loaded()
+                    this.limit++
+                    if(res.data.length / EACH_LEN < 1) {
+                        $state.complete()
+                    }
+                } else {
+                    // 끝 지정(No more data)
+                    $state.complete()
+                }
+                }, 400)
+            })
+        },
+
         Loading() {
               if (this.isLoading) {
                   setTimeout(this.delayfinish, 200);
