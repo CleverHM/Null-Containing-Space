33217a4f4c4067a2231d99c1ca55687c41eaa0d7
yusoojeong <yusj1231@gmail.com>
2020-08-13 17:22:01 +0900

    infinite loading follow page / S03P13D105-57

--- frontend/src/views/user/followinglist.vue
+++ frontend/src/views/user/followinglist.vue
@@ -1,110 +1,136 @@
 <template>
-    <div id="followList">
-        <Navbar />
-            <nav class="default-tabs">
-              <div class="default-tabs-item" :class="{'tabs-item_active':isCurrent}" @click="handleClick">
-                <button> 팔로워 </button> <!-- active tab -->
-                <div class="default-tabs-active-line"></div>
-              </div>
-              <div class="default-tabs-item" :class="{'tabs-item_active':!isCurrent}" @click="handleClick">
-                <button> 팔로잉 </button>
-                <div class="default-tabs-active-line"></div>
-              </div>
-            </nav>
-            
-            <div class="content">
-              <div v-if="isCurrent">
-                
-                <followItem :User="User" v-for="User in List" :key="User.nickname"/>
+  <div id="followList">
+    <Navbar />
+      <nav class="default-tabs">
+        <div class="default-tabs-item" :class="{'tabs-item_active':isCurrent}" @click="handleClick">
+          <button> 팔로워 </button> <!-- active tab -->
+          <div class="default-tabs-active-line"></div>
+        </div>
+        <div class="default-tabs-item" :class="{'tabs-item_active':!isCurrent}" @click="handleClick">
+          <button> 팔로잉 </button>
+          <div class="default-tabs-active-line"></div>
+        </div>
+      </nav>
+      
+      <div class="content">
+        <div v-if="isCurrent">
+          
+          <followItem :User="User" v-for="User in List" :key="User.nickname"/>
 
-              </div>
-              <div v-else >
-                <followItem :User="User" v-for="User in List" :key="User.nickname"/>
-                
-              </div>
-            </div>
+        </div>
+        <div v-else >
+          <followItem :User="User" v-for="User in List" :key="User.nickname"/>
+          
+        </div>
+        
+        <!-- 무한스크롤 -->
+        <infinite-loading 
+          @infinite="infiniteHandler" 
+          ref="InfiniteLoading"
+          spinner="waveDots">
+          <div slot="no-results" style="font-size: 14px; padding: 10px 0px; margin-top:20px;">아직 {{ currentTab }}(이/가) 존재하지 않습니다.</div>
+          <div slot="no-more" style="display: none;"></div>
+        </infinite-loading>
+      </div>
 
-    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from '../../components/common/Navigation.vue'
 import followItem from '@/components/user/followItem.vue'
 import http from "@/util/http-common.js";
+import InfiniteLoading from 'vue-infinite-loading';
+
+const storage = window.sessionStorage;
 
 export default {
-    name: "followList",
-    props: [
-        'PageName',
-        'nickname',
-    ],
-    components: {
-        Navbar,
-        followItem
-    },
-    created() {
-      this.getfollow()
-       
-        
-
-    },
-    data() {
-        return {
-            isCurrent: true,
-            currentTab: "",
-            followFlag : 1,
-            List: [],
+  name: "followList",
+  props: [
+    'PageName',
+    'nickname',
+  ],
+  components: {
+    Navbar,
+    followItem,
+    InfiniteLoading,
+  },
+  created() {
+    this.getfollow();
+  },
+  data() {
+    return {
+      isCurrent: true,
+      currentTab: '팔로워',
+      limit: 1,
+      followFlag : 1,
+      List: [],
+    }
+  },
+  methods : {
+    getfollow() {
+      if (this.PageName == 'following') {
+          this.isCurrent = false
+          this.currentTab = '팔로잉'
+          this.followFlag = 1
+      }
+      else {
+        this.isCurrent = true
+        this.currentTab = '팔로워'
+        this.followFlag = 2
         }
     },
-    methods : {
-        getfollow() {
-           // 팔로잉 : 1, 팔로워 ; 2
-          if (this.PageName == 'following') {
-              this.isCurrent = false
-              this.currentTab = '팔로잉'
-              this.followFlag = 1
-          }
-          else {
-            this.isCurrent = true
-            this.currentTab = '팔로워'
-            this.followFlag = 2
+
+    dataReceive($state) {
+      const EACH_LEN = 10
+
+      var InputData = new FormData()
+      InputData.append("nickname", this.nickname)
+      InputData.append("flag", this.followFlag)
+      InputData.append("pagenum", this.limit)
+      http.post("/follow/user/list", InputData)
+      .then((res) => {
+        setTimeout(() => {
+          if(res.data.length) {
+            this.List = [...this.List, ...res.data]
+            $state.loaded()
+            this.limit++
+            if(res.data.length / EACH_LEN < 1) {
+              $state.complete()
             }
-          var InputData = new FormData()
-          InputData.append("nickname", this.nickname)
-          InputData.append("flag", this.followFlag)
-          console.log(this.nickname, this.followFlag)
-          http.post("/follow/user/list", InputData)
-          .then(({data}) => {
-              this.List = data
-              console.log(this.List)
-          })
-        },
-        handleClick(event) {
-          console.log(event.target.innerText)
-          this.currentTab = event.target.innerText;
-          if (this.currentTab == '팔로잉') {
-            this.isCurrent = false
-            this.currentTab = '팔로잉'
-            this.followFlag = 1
+          } else {
+            // 끝 지정(No more data)
+            $state.complete()
           }
-          else {
-            this.isCurrent = true
-            this.currentTab = '팔로워'
-            this.followFlag = 2
-          }
-          var InputData = new FormData()
-          console.log(this.followFlag)
-          InputData.append("nickname", this.nickname)
-          InputData.append("flag", this.followFlag)
-          console.log(this.nickname, this.followFlag)
-          http.post("/follow/user/list", InputData)
-          .then(({data}) => {
-              this.List = data
-              console.log(this.List)
-          })
-          
-        },
-    }
+        }, 400)
+
+      })
+        
+    },
+
+    // 무한 스크롤
+    infiniteHandler($state) {
+      this.dataReceive($state);
+    },
+
+    handleClick(event) {
+      if (this.currentTab != event.target.innerText) {
+        this.currentTab = event.target.innerText;
+        this.$refs.InfiniteLoading.stateChanger.reset(); 
+        this.List = []
+        this.limit = 1
+        if (this.currentTab == '팔로잉') {
+          this.isCurrent = false
+          this.followFlag = 1
+        }
+        else {
+          this.isCurrent = true
+          this.followFlag = 2
+        }
+      }
+    },
+
+  }
     
 }
 </script>
@@ -116,25 +142,25 @@ export default {
   /* margin: 0 auto; */
 }
 .default-tabs-item {
-    display: inline-block;
-    padding-bottom: 8px;
-    font-size: 16px;
-    letter-spacing: 0.8px;
-    color: gray;
-    text-decoration: none;
-    text-align: center;
-    border: none;
-    background-color: transparent;
-    border-bottom: 2px solid transparent;
-    cursor: pointer;
-    -webkit-transition: all 0.25s;
-    transition: all 0.25s;
-    width: 50%;
+  display: inline-block;
+  padding-bottom: 8px;
+  font-size: 16px;
+  letter-spacing: 0.8px;
+  color: gray;
+  text-decoration: none;
+  text-align: center;
+  border: none;
+  background-color: transparent;
+  border-bottom: 2px solid transparent;
+  cursor: pointer;
+  -webkit-transition: all 0.25s;
+  transition: all 0.25s;
+  width: 50%;
 }
 .tabs-item_active{
-      outline: none;
-      border-bottom: 2px solid gray;
-      color: black;
+  outline: none;
+  border-bottom: 2px solid gray;
+  color: black;
 }
 .default-tabs-item button {
   cursor: pointer;
@@ -142,27 +168,27 @@ export default {
   outline: 0;
 }
 .default-tabs-item_active {
-      color: black;
+  color: black;
 }
 .default-tabs-item:hover {
-      border-bottom: 2px solid gray;
-      color: black;
+  border-bottom: 2px solid gray;
+  color: black;
 }
 .default-tabs-item:focus {
-      outline: none;
-      border-bottom: 2px solid gray;
-      color: black;
+  outline: none;
+  border-bottom: 2px solid gray;
+  color: black;
 }
 .default-tabs-item:visited{
-      outline: none;
-      border-bottom: 2px solid gray;
-      color: black;
+  outline: none;
+  border-bottom: 2px solid gray;
+  color: black;
 }
 .default-tabs-item:first-child {
-      margin-left: 0;
+  margin-left: 0;
 }
 .default-tabs-item:last-child {
-      margin-right: 0;
+  margin-right: 0;
 }
 
 .content {
