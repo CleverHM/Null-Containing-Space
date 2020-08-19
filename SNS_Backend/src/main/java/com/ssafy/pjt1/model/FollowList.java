6701193b14f930fc8fd38ac2f145468b39a5a01f
Daeheum <conquerer1209@gmail.com>
2020-08-19 17:20:19 +0900

    팔로우  컨트롤러 수정

--- SNS_Backend/src/main/java/com/ssafy/pjt1/model/FollowList.java
+++ SNS_Backend/src/main/java/com/ssafy/pjt1/model/FollowList.java
@@ -1,29 +1,40 @@
 package com.ssafy.pjt1.model;
 
 public class FollowList {
-	private byte[] file;
-	private String nickname;
-	
-	public FollowList(byte[] file, String nickname) {
-		this.file = file;
-		this.nickname = nickname;
-	}
+    private int fid;
+    private byte[] file;
+    private String nickname;
 
-	public byte[] getFile() {
-		return file;
-	}
+    public FollowList(int fid, byte[] file, String nickname) {
 
-	public void setFile(byte[] file) {
-		this.file = file;
-	}
+        this.fid = fid;
+        this.file = file;
+        this.nickname = nickname;
+    }
 
-	public String getNickname() {
-		return nickname;
-	}
+    public int getFid() {
+        return fid;
+    }
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	
-	
+    public void setFid(int fid) {
+        this.fid = fid;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    
+    
 }
